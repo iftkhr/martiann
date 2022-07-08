89ef98e3510c2a426c0641c7c81d56dde0394196
Iftakhar Kaunain Ashhar <iftakhar.ashhar@gmail.com>
2022-07-08 10:20:55 +0530

    add infinite scroll functionality

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,11 +1,39 @@
+import { useState } from "react";
 import Post from "../Post/Post";
 import "./Posts.css";
 
 function Posts({ posts, user }) {
-	console.log(posts);
+	const [postsToAdd, setPostsToAdd] = useState(10);
+	const [postsToLoad, setPostsToLoad] = useState([
+		...posts.slice(0, postsToAdd),
+	]);
+	console.log(postsToAdd);
+
+	var lastScrollTop = 0;
+
+	window.addEventListener(
+		"scroll",
+		function () {
+			var st = window.pageYOffset || document.documentElement.scrollTop;
+			if (st > lastScrollTop) {
+				var scrollPercentage = Math.floor(
+					100 * (window.pageYOffset / document.body.clientHeight)
+				);
+				if (scrollPercentage > 70 && scrollPercentage % 10 === 0 && window.pageYOffset && postsToAdd <= posts.length) {
+					setPostsToAdd(postsToAdd + 5);
+					setPostsToLoad([...posts.slice(0, postsToAdd)]);
+					console.log(postsToAdd);
+					console.log(scrollPercentage);
+				}
+			}
+			lastScrollTop = st <= 0 ? 0 : st;
+		},
+		false,
+	);
+
 	return (
 		<div className="posts-container">
-			{posts.map((post) => {
+			{postsToLoad.map((post) => {
 				return (
 					<div className="post" key={post.id}>
 						<Post post={post} user={user} />
