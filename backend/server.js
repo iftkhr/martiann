27e0526b2022f21a46cf8e44b57507945b4d1486
Iftakhar Kaunain Ashhar <iftakhar.ashhar@gmail.com>
2022-07-08 18:50:28 +0530

    edits

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const database = mysql.createConnection({
 });
 
 const app = express();
-const port = process.env.SERVER_PORT || 3306;
+const port = process.env.PORT || 3306;
 
 app.use(cors());
 app.use(express.json());
@@ -47,13 +47,11 @@ app.get("/api/posts", (req, res) => {
 	});
 });
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "build", "index.html"));
-	});
-}
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
 
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
