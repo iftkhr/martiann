fed8bbfd31462300bbd78784d396d442e24787b0
Iftakhar Kaunain Ashhar <iftakhar.ashhar@gmail.com>
2022-07-08 17:50:32 +0530

    update backend

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,59 @@
+const dotenv = require("dotenv");
+const mysql = require("mysql");
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-const db = require("./database");
 
-require("dotenv").config();
+dotenv.config();
+
+const database = mysql.createConnection({
+	host: process.env.host,
+	user: process.env.user,
+	password: process.env.password,
+	port: process.env.port,
+	database: process.env.database,
+});
 
 const app = express();
 const port = process.env.PORT || 3306;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../build")));
 
-db.connect((err) => {
+database.connect((err) => {
 	if (err) throw err;
 	console.log("SQL database connection established successfully!");
 });
 
-const mainRouter = require("./routes");
-const postsRouter = require("./routes");
-const usersRouter = require("./routes");
-
-app.use("/", mainRouter);
-app.use("/api/posts", postsRouter);
-app.use("/api/users", usersRouter);
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../build", "index.html"));
+app.get("/api", (req, res) => {
+	res.send("Go to /api/users for users and /api/posts for posts");
 });
+
+app.get("/api/users", (req, res) => {
+	database.query("SELECT * FROM users", (err, result) => {
+		if (err) {
+			console.log(err);
+		}
+		res.send(result);
+	});
+});
+
+app.get("/api/posts", (req, res) => {
+	database.query("SELECT * FROM posts", (err, result) => {
+		if (err) {
+			console.log(err);
+		}
+		res.send(result);
+	});
+});
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "build")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "build", "index.html"));
+	});
+}
 
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
