b9f95c2d10a267cca530fa2e5fc1c04f0dc95044
Iftakhar Kaunain Ashhar <iftakhar.ashhar@gmail.com>
2022-07-08 19:02:37 +0530

    edits

--- backend/server.js
+++ backend/server.js
@@ -47,11 +47,12 @@ app.get("/api/posts", (req, res) => {
 	});
 });
 
-app.use(express.static(path.join(__dirname, "../build")));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "build", "index.html"));
+	});
+}
 
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
